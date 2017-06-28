5e200c153a9755f5f53cf3afb51d4be7f7f351d3
Lyra Joyce Ronquillo <ljronki@gmail.com>
2017-06-28 16:25:29 +0800

    Update itemdetails.php

--- application/views/modals/itemdetails.php
+++ application/views/modals/itemdetails.php
@@ -1,1 +1,36 @@
-itemdet
+<button class="fa fa-info" data-toggle="modal" data-target="#view"></button>
+                       <!-- Modal-->
+                        <div class="modal fade" id="view" role="dialog">
+                    <div class="modal-dialog">
+                    
+                      <!-- Modal content-->
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4 class="modal-title" align="center"><b>Edit Inventory<b></h4>
+                        </div>
+                        <div class="modal-body" align="center">
+
+                        <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Item no.</th>
+                  <th>Serial no.</th>
+                  <th>Account Code</th>
+                  <th>Expiration Date</th>
+                  <th>Description</th>
+                  
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
