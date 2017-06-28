8e61ec96e8c80b9a2c0e8baceb04a5dcbc45bfad
webtek-hein <2151287@slu.edu.ph>
2017-06-28 11:20:13 +0800

    initiial integration of adminlte template

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,7 +5,6 @@ class Dashboard extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('templates/header');
-		$this->load->view('dashboard');
 		$this->load->view('templates/footer');
 
 	}
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,2 +1,43 @@
+ <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 2.2.3 -->
+<script src="http://localhost/app/assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="http://localhost/app/assets/js/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.6 -->
+<script src="http://localhost/app/assets/js/bootstrap.min.js"></script>
+<!-- Morris.js charts -->
+<script src="http://localhost/app/assets/js/raphael-min.js"></script>
+<script src="http://localhost/app/assets/plugins/morris/morris.min.js"></script>
+<!-- Sparkline -->
+<script src="http://localhost/app/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+<!-- jvectormap -->
+<script src="http://localhost/app/assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="http://localhost/app/assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="http://localhost/app/assets/plugins/knob/jquery.knob.js"></script>
+<!-- daterangepicker -->
+<script src="http://localhost/app/assets/js/moment.min.js"></script>
+<script src="http://localhost/app/assets/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- datepicker -->
+<script src="http://localhost/app/assets/plugins/datepicker/bootstrap-datepicker.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="http://localhost/app/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<!-- Slimscroll -->
+<script src="http://localhost/app/assets/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="http://localhost/app/assets/plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="http://localhost/app/assets/dist/js/app.min.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="http://localhost/app/assets/dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="http://localhost/app/assets/dist/js/demo.js"></script>
 </body>
 </html>
