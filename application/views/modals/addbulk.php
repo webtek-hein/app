f9a6c43224dc4e94527f37be19e9f6da4530a648
Lyra Joyce Ronquillo <ljronki@gmail.com>
2017-06-28 16:19:13 +0800

    Update addbulk.php

--- application/views/modals/addbulk.php
+++ application/views/modals/addbulk.php
@@ -1,1 +1,23 @@
-addbulk 
+<button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal1">Add Bulk Items</button>
+         <!-- Modal-->
+        <div class="modal fade" id="myModal1" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title" align="center"><b>How many items?<b></h4>
+        </div>
+        <div class="modal-body" align="center">
+
+          <input type="text" class="InputBox" name="manyitems" value="" >
+            
+        </div>
+        <div class="modal-footer">
+         <button type="button" class="btn btn-default" id="save" data-dismiss="modal">Enter</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
