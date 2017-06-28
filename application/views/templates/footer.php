bb98d0b3ae59a4f16c493aac9e88c727f139331b
webtek-hein <2151287@slu.edu.ph>
2017-06-28 12:23:59 +0800

    added return view

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,75 @@
+    </section>
+      
+ <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/admin.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Admin</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+    
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+    
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-book"></i> <span>Inventory</span>
+          </a>
+        </li>
+    
+    
+    
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-building"></i>
+            <span>Department</span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
+            <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
+            <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
+            <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
+            <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
+            <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
+          </ul>
+        </li>
+    
+    
+        <li class="treeview">
+          <a href="http://localhost/app/rt">
+            <i class="fa fa-reply"></i> <span>Return</span>
+          </a>
+        </li>
+    
+    
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-table"></i> <span>Logs</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+      <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Increase</a></li>
+            <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Decrease</a></li>
+            <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Return log</a></li>
+          </ul>
+        </li>
+       
+    <!-- /.sidebar -->
+  </aside>
  <div class="control-sidebar-bg"></div>
 </div>
 <!-- ./wrapper -->
