26715d74022f8e1546f21a099d69ec6b1081577c
webtek-hein <2151287@slu.edu.ph>
2017-06-28 13:27:12 +0800

    remove tfoot

--- application/views/return.php
+++ application/views/return.php
@@ -34,18 +34,6 @@
                   </td>
                 </tr>
                 </tbody>
-                <tfoot>
-                <tr>
-                  <th>Serial no.</th>
-                  <th>Item Name</th>
-                  <th>Account Code</th>
-                  <th>Date</th>
-                  <th>Supplier</th>
-                  <th>Department</th>
-                  <th>Reason</th>
-                  <th>Action</th>
-                </tr>
-                </tfoot>
               </table>
             </div>
             <!-- /.box-body -->
@@ -56,62 +44,4 @@
       </div>
       <!-- /.row -->
     </section>
-    <!-- /.content --><section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-            <!-- /.box-header -->
-          <div class="box">
-          <div class="box-header">
-              <h3 class="box-title">Return</h3>
-            </div>
-            <div class="box-body">
-              <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>Serial no.</th>
-                  <th>Item Name</th>
-                  <th>Account Code</th>
-                  <th>Date</th>
-                  <th>Supplier</th>
-                  <th>Department</th>
-                  <th>Reason</th>
-                  <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td><input type="submit" name="Replace" value="Replace" class="Replace">
-                      <input type="submit" name="Ignore" value="Ignore" class="Ignore">
-                  </td>
-                </tr>
-                </tbody>
-                <tfoot>
-                <tr>
-                  <th>Serial no.</th>
-                  <th>Item Name</th>
-                  <th>Account Code</th>
-                  <th>Date</th>
-                  <th>Supplier</th>
-                  <th>Department</th>
-                  <th>Reason</th>
-                  <th>Action</th>
-                </tr>
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->v
+   
