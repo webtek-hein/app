32560c78fa11a3ab67f8313404c0a417001f52a9
Lyra Joyce Ronquillo <ljronki@gmail.com>
2017-06-28 16:21:24 +0800

    Update editinventory.php

--- application/views/modals/editinventory.php
+++ application/views/modals/editinventory.php
@@ -1,1 +1,49 @@
-editinventory
+<button class="fa fa-pencil" data-toggle="modal" data-target="#edit"></button>
+                               <!-- Modal-->
+                        <div class="modal fade" id="edit" role="dialog">
+                    <div class="modal-dialog">
+                    
+                      <!-- Modal content-->
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4 class="modal-title" align="center"><b>Edit Inventory<b></h4>
+                        </div>
+                        <div class="modal-body" align="center">
+
+                         <form name="additem" method="post" action="">
+                          <table border="0" width="500" align="center" class="table">
+                          <tr>
+                              <td>Item Name</td>
+                              <td><input type="text" class="InputBox" name="Supplier" value=""></td>
+                            </tr>
+                            <tr>
+                              <td>Description</td>
+                              <td><input type="text" class="InputBox" name="Supplier" value=""></td>
+                            </tr>
+                          <tr>
+                              <td>Account Code</td>
+                              <td>
+                              <select class="accountcode">
+                                <option value="">...............</option>
+                                <option value="">...............</option>
+                                <option value="">...............</option>
+                                <option value="">...............</option>
+                              </select>
+                              </td>
+                            </tr>
+                            <tr>
+                              <td>Unit</td>
+                              <td><input type="text" class="unit" name="Supplier" value=""></td>
+                            </tr>
+                          </table>
+                        </form>
+                            
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-default" id="save1" data-dismiss="modal">Save</button>
+                          <button type="button" class="btn btn-default" id="cancel1" data-dismiss="modal">Cancel</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
