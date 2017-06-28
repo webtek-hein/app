819baac9458a6c7993d4b332771b4a8520643168
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2017-06-28 11:51:56 +0800

    added return log

--- application/views/returnlog.php
+++ application/views/returnlog.php
@@ -1,1 +1,48 @@
-returnlog
+ <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Return log</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Serial</th>
+                  <th> Item name</th>
+                  <th> Date</th>
+                  <th> Date receive</th>
+                  <th> Unit Cost</th>
+					<th> Supplier </th>
+					<th>User</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Trident</td>
+                  <td>Internet
+                    Explorer 4.0
+                  </td>
+                  <td>Win 95+</td>
+                  <td> 4</td>
+                  <td>X</td>
+                  <td> 4</td>
+                  <td>X</td>
+                </tr>
+                </tbody>
+                <tfoot>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
