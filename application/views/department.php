bca0f2542f356cdc2224b0c36d43d25f28f65d60
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2017-06-29 10:37:48 +0800

    added department

--- application/views/department.php
+++ application/views/department.php
@@ -1,1 +1,53 @@
 department
+
+ <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Department</h3><br>
+			  <button type= "button" class="btn btn-success">Summary of Items</button>			  
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Item name</th>
+                  <th> Account Code</th>
+                  <th> Official Receipt</th>
+				  <th> Delivery Date</th>
+                  <th> Date received</th>
+                  <th> Quantity</th>
+					<th> Received by </th>
+					<th>Cost</th>
+					<th> Unit</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+				  <td> </td>
+                  <td> </td>
+                </tr>
+                </tbody>
+                <tfoot>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
