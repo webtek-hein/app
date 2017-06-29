5bfa2ecce2c5df10772ea7596daa54622f588af0
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2017-06-29 10:38:50 +0800

    added summary of items

--- application/views/modals/summaryofitems.php
+++ application/views/modals/summaryofitems.php
@@ -1,1 +1,53 @@
 sumofitems
+<button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">Summary of item</button>
+         <!-- Modal --> 
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title" align="center"><b>Summary of Item<b></h4>
+        </div>
+        <div class="modal-body">
+
+             <form name="additem" method="post" action="">
+      <table border="0" width="500" align="center" class="table">
+        <tr>
+          <td>Item Name</td>
+          <td><input type="text" class="InputBox" name="ItemName" value=""></td>
+        </tr>
+        <tr>
+          <td>Account Code</td>
+          <td>
+          <select class="accountcode">
+            <option value="">...............</option>
+            <option value="">...............</option>
+            <option value="">...............</option>
+            <option value="">...............</option>
+          </select>
+          </td>
+        </tr>
+        <tr>
+          <td>Quantity</td>
+          <td><input type="int" class="InputBox" name="Quantity" value=""></td>
+        </tr>
+        
+          <td>Unit</td>
+          <td><input type="text" class="InputBox" name="Unit" value=""></td>
+        </tr>
+        
+      </table>
+    </form>
+            
+            
+        </div>
+        <div class="modal-footer">
+         <button type="button" class="btn btn-default" id="save1" data-dismiss="modal">Save</button>
+          <button type="button" class="btn btn-default" id="cancel1" data-dismiss="modal">Cancel</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
