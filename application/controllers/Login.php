15e6586d1d5f729825ed2b185d434b69a86cf624
markr <markryanc23@gmail.com>
2017-06-30 12:35:49 +0800

    add functions

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,33 +6,77 @@ class Login extends CI_Controller {
 
 	public function __construct() {
 	parent::__construct();
-		$this->load->model('user_db');
-	}
-	public function index()
-	{
-		$this->load->view('login');
-	}
-	public function user_login_process() 
-	{
-		$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
-		$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
-		if ($this->form_validation->run() == FALSE) {
-			if(isset($this->session->userdata['logged_in'])){
-				$this->load->view('dashboard');
-			}else {
-				$username = $this->input->post('username');
-				$password = $this->input->post('password');
-				$result = $this->user_db->login($username,$password);
-				if($result !=false){
-					$user_data = array('username' => $result[5]->user_name);
-					$this->session->set_userdata('logged_in',$user_data);
-					$this->load->view('dashboard');
-				} else {
-					$data = array('error_message' => 'Invalid Username or Password');
-					$this->load->view('login', $data);
-				}
+        // Load form helper library
+        $this->load->helper('form');
+// Load url helper library
+        $this->load->helper('url');
+// Load form validation library
+        $this->load->library('form_validation');
 
-			}
-		}
+// Load session library
+        $this->load->library('session');
+
+// Load database
+        $this->load->model('user_db');
+    }
+
+// Show login page
+    public function index()
+    {
+        $this->load->view('login');
+    }
+
+    // Check for user login process
+
+    public function user_login_process() {
+
+        $this->form_validation->set_rules('username', 'Username', 'trim|required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if ($this->form_validation->run() == FALSE) {
+            if(isset($this->session->userdata['logged_in'])){
+                $this->load->view('templates/header');
+            }else{
+                $this->load->view('login');
+            }
+        } else {
+            $data = array(
+                'username' => $this->input->post('username'),
+                'password' => $this->input->post('password')
+            );
+            $result = $this->user_db->login($data);
+            if ($result == TRUE) {
+
+                $username = $this->input->post('username');
+                $result = $this->user_db->read_user_information($username);
+                if ($result != false) {
+                    $session_data = array(
+                        'username' => $result[0]->username,
+                        'firstname' => $result[0]->first_name,
+                        'lastname' => $result[0]->last_name,
+                    );
+// Add user data in session
+                    $this->session->set_userdata('logged_in', $session_data);
+                    $this->load->view('templates/header');
+                }
+            } else {
+                $data = array(
+                    'error_message' => 'Invalid Username or Password'
+                );
+                $this->load->view('login', $data);
+            }
+        }
+    }
+
+// Logout from admin page
+    public function logout() {
+
+// Removing session data
+        $sess_array = array(
+            'username' => ''
+        );
+        $this->session->unset_userdata('logged_in', $sess_array);
+        $data['message_display'] = 'Successfully Logout';
+        $this->load->view('login_form', $data);
 	}
 }
--- application/models/User_db.php
+++ application/models/User_db.php
@@ -3,15 +3,41 @@ class User_db extends CI_Model {
 
     public function __construct()
     {
+        $this->load->database();
     }
 
-	public function login($username,$password)
-	{
-       $query = $this->db->get_where('user', array('username'=>$username,'password'=>$password));
-       if($query->num_rows == 1){
-        return true;
-       }else{
-        return false;
-       }
-	}
+// Read data using username and password
+    public function login($data) {
+
+        $condition = "username =" . "'" . $data['username'] . "' AND " . "password =" . "'" . $data['password'] . "'";
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->where($condition);
+        $this->db->limit(1);
+        $query = $this->db->get();
+
+        if ($query->num_rows() == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+// Read data from database to show data in admin page
+    public function read_user_information($username) {
+
+        $condition = "username =" . "'" . $username . "'";
+        $this->db->select('*');
+        $this->db->from('user');
+        $this->db->where($condition);
+        $this->db->limit(1);
+        $query = $this->db->get();
+
+        if ($query->num_rows() == 1) {
+            return $query->result();
+        } else {
+            return false;
+        }
+    }
+
 }
