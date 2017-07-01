d8b6727d98524044b1d0d0d32aeeff610aa1c84f
markr <markryanc23@gmail.com>
2017-07-01 10:56:30 +0800

    update

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,7 @@ class Dashboard extends CI_Controller {
 	public function index()
 	{
 			$this->load->view('templates/header');
+            $this->load->view('dashboard');
 			$this->load->view('templates/footer');
 	}
 }
