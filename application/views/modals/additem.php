636c41b4ac9b0b4c48fb739bb6f02b1b43ab10b9
gloridhelgoyo <gloridhelgoyo@yahoo.com>
2017-07-11 14:13:42 +0800

    required input

--- application/views/modals/additem.php
+++ application/views/modals/additem.php
@@ -18,17 +18,17 @@
       <table border="0" width="500" align="center" class="table">
         <tr>
           <td>Item Name</td>
-          <td><input type="text" class="InputBox" name="Item_Name" value=""></td>
+          <td><input type="text" class="InputBox" name="Item_Name" value="" required="required"></td>
         </tr>
         <tr>
           <td>Item Description</td>
-          <td><input type="text" class="InputBox" name="Description" value=""></td>
+          <td><input type="text" class="InputBox" name="Description" value="" required="required"></td>
         </tr>
         <tr>
           <td>Account Code</td>
           <td>
           <!-- Display description, option value is account id eg. 1-07-01-010 for land -->
-          <select name="AccountCode" class="accountcode">
+          <select name="AccountCode" class="accountcode" required="required">
             <?php foreach ($accountcodes as $ac_record): ?>
               <option value="<?php echo $ac_record['ac_id']; ?>"><?php echo $ac_record['account_code']," ", $ac_record['description']; ?></option>
             <?php endforeach; ?>
@@ -38,35 +38,35 @@
 
         <tr>
           <td>Official Receipt</td>
-          <td><input type="int" class="InputBox" name="OfficialReceipt" value=""></td>
+          <td><input type="int" class="InputBox" name="OfficialReceipt" value="" required="required"></td>
         </tr>
 	<tr>
           <td>Received By</td>
-          <td><input type="text" class="InputBox" name="ReceivedBy" value=""></td>
+          <td><input type="text" class="InputBox" name="ReceivedBy" value="" required="required"></td>
         </tr>
 
         <tr>
           <td>Quantity</td>
-          <td><input type="int" class="InputBox" name="Item_Quantity" value=""></td>
+          <td><input type="int" class="InputBox" name="Item_Quantity" value="" required="required"></td>
         </tr>
 
       <tr>
           <td>Supplier</td>
-          <td><input type="text" class="InputBox" name="Supplier_Name" value=""></td>
+          <td><input type="text" class="InputBox" name="Supplier_Name" value="" required="required"></td>
         </tr>
 
         <tr>
           <td>Delivery Date</td>
-          <td><input type="date" class="datereceived" name="datedelivered" value=""></td>
+          <td><input type="date" class="datereceived" name="datedelivered" value="" required="required"></td>
         </tr>
         <tr>
           <td>Date Received</td>
-          <td><input type="date" class="datereceived" name="datereceived" value=""></td>
+          <td><input type="date" class="datereceived" name="datereceived" value="" required="required"></td>
         </tr>
         <tr>
           <td>Unit</td>
           <td>
-            <select name="Unit" class="unit">
+            <select name="Unit" class="unit" required="required">
               <option value="piece">piece</option>
               <option value="box">box</option>
               <option value="set">set</option>
@@ -80,11 +80,11 @@
         </tr>
         <tr>
           <td>Unit Cost</td>
-          <td><input type="int" class="InputBox" name="Cost" value=""></td>
+          <td><input type="int" class="InputBox" name="Cost" value="" required="required"></td>
         </tr>
         <tr>
           <td>Expiration Date</td>
-          <td><input type="date" class="expdate" name="ExpirationDate" value=""></td>
+          <td><input type="date" class="expdate" name="ExpirationDate" value="" required="required"></td>
         </tr>
       </table> 
         </div>
