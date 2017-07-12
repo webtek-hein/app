a8810620495680321a05db642337225547b1fe3f
JoyCabildo <Joy_Cabildo24@yahoo.com>
2017-07-12 15:04:07 +0800

    latest return log

--- application/models/InventoryModel.php
+++ application/models/InventoryModel.php
@@ -74,11 +74,14 @@ natural join account_code natural join department");
     {
         $dbase=$this->load->database('inventory', TRUE);
 
-$query = $dbase->query("Select serial,item_name,date,date_rec,concat(first_name,last_name) as user,unit_cost,department,user_distribute as employee from increase_log
-left join item_detail on item_detail.item_det_id = increase_log.item_det_id
-left join user on user.user_id = increase_log.user_id
-left join item on item.item_id = item_detail.item_id
-natural join department left join distribution on item_detail.dist_id = distribution.dist_id");
+$query = $dbase->query("select serial,item_name,account_code,return_log.date,supplier,department,reason from return_log
+natural join department
+left join user on user.user_id = return_log.user_id
+left join item_detail on item_detail.item_det_id = return_log.item_det_id
+left join distribution on distribution.dist_id = item_detail.dist_id
+natural join account_code
+natural join item");
+
         return $query->result_array();
     }
     public function get_return_log()
