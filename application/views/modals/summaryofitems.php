08aad045d9919caf65cc78e801ca52dae849c677
lyraronquillo <ljronki@gmail.com>
2017-07-13 11:35:33 +0800

    edit modal

--- application/views/modals/summaryofitems.php
+++ application/views/modals/summaryofitems.php
@@ -5,7 +5,7 @@
   <div class="modal fade" id="summary" role="dialog">
     <div class="modal-dialog">
     
-<div class="container" style="background-color:white; width:1520px; height: 550px; margin-left: -450px; size:50px;">
+<div class="container" style="background-color:white; width:1520px; height: 550px; margin-left: -455px; size:50px;">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
           <h4 class="modal-title" align="center"><b>Summary of Item<b></h4>
