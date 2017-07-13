ad951438be010dcb13dd34ca8b74623fad8781bb
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2017-07-13 13:08:33 +0800

    remove dashboard.js

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -38,7 +38,6 @@
 <!-- AdminLTE App -->
 <script src="<?php echo base_url() ?>assets/dist/js/app.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url() ?>assets/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo base_url() ?>assets/dist/js/demo.js"></script>
 </body>
