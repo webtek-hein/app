8d4bd60cb446cf18216a1be02ed616d1ef75cf84
Rusell Bayote <rusellbayote@gmail.com>
2017-07-14 11:47:03 +0800

    return item update

--- application/models/Return_model.php
+++ application/models/Return_model.php
@@ -13,11 +13,14 @@ class Return_model extends CI_Model {
     }
 
     //not fully working
-    public function return_items_to_inventory($serial)
+    public function return_items_to_inventory($item)
     {
         $inventory = $this->load->database('inventory', TRUE);
-        $query1 = $inventory->query("UPDATE distribution SET quantity=quantity-1 WHERE dist_id=(SELECT dist_id FROM item_detail WHERE serial = $serial)");
-        $query2 = $inventory->query("UPDATE item_detail SET dist_id=NULL WHERE serial = $serial");
-        $query3 = $inventory->query("UPDATE item SET quantity=quantity+1 WHERE item_id = (SELECT item_id FROM item_detail WHERE serial = $serial)");
+        $query1 = $inventory->query("UPDATE distribution SET quantity=quantity-1 WHERE dist_id=(SELECT dist_id FROM item_detail WHERE item_det_id = $item)");
+        $query2 = $inventory->query("UPDATE item_detail SET dist_id=NULL WHERE item_det_id = $item");
+        $query3 = $inventory->query("UPDATE item SET quantity=quantity+1 WHERE item_id = (SELECT item_id FROM item_detail WHERE item_det_id = $item)");
+
+        //for return_log
+        //$query4 = $inventory->query("INSERT INTO return_log (reason, item_det_id, dept_id, return_person, status, user_id) VALUES () ");
     }
 }
