8af06a9838a6b89a7fd1dae3dc80de7f948e43b8
gloridhelgoyo <gloridhelgoyo@yahoo.com>
2017-07-15 15:28:39 +0800

    required add bulk

--- application/views/modals/addbulk.php
+++ application/views/modals/addbulk.php
@@ -1,9 +1,15 @@
+<?php $position = ($this->session->userdata['logged_in']['position']); ?>
 
-<script src="<?php echo base_url() ?>assets/js/addbulk.js"></script>
 
+<?php echo validation_errors(); ?>
+      <?php echo form_open($position.'/inventory/addbulk'); ?>
         <!-- Modal -->
 <div class="modal fade" id="myModal1" role="dialog">
     <div class="modal-dialog">
+    <?php if($this->session->flashdata('msg')): ?>
+        <p><?php echo $this->session->flashdata('msg'); ?></p>
+      <?php endif; ?>
+        <div class="modal-body">
 
     <!-- Modal content-->
     <div class="container" style="background-color:white; width:1350px; height: auto; margin-left: -368px; size:50px; overflow-x:scroll;">
@@ -12,12 +18,16 @@
 <div class="modal-body">
        
        <div id="wrapper">
-<table align='center' cellspacing=2 cellpadding=5 id="data_table" border=1>
+<table align='center'  cellspacing=2 cellpadding=5 id="data_table" border=1>
 <tr>
-<th >Item Name</th>
+<th>Item Name</th>
+<th>Item Description</th>
 <th>Account Code</th>
+<th>Official Receipt</th>
+<th>Received By</th>
 <th>Quantity</th>
-<th>Supplier</th>
+<th>Supplier Name</th>
+<th>Date Delivered</th>
 <th>Date Received</th>
 <th>Unit</th>
 <th>Cost</th>
@@ -25,14 +35,31 @@
 </tr>
 
 <tr>
-<td><input type="text" id="new_itemname"></td>
-<td><input type="text" id="new_accountcode"></td>
-<td><input type="text" id="new_qty"></td>
-<td><input type="text" id="new_supplier"></td>
-<td><input type="text" id="new_datereceived"></td>
-<td><input type="text" id="new_unit"></td>
-<td><input type="text" id="new_cost"></td>
-<td><input type="text" id="new_expdate"></td>
+<td><input type="text" class="InputBox" name="Item_Name2"  required="required"></td>
+<td><input type="text" class="InputBox" name="Description2"  required="required"></td>
+<td>
+<select name="AccountCode" class="accountcode" required="required">
+            <?php foreach ($accountcodes as $ac_record): ?>
+              <option value="<?php echo $ac_record['ac_id']; ?>"><?php echo $ac_record['account_code']," ", $ac_record['description']; ?></option>
+            <?php endforeach; ?></td>
+<td><input type="text" class="InputBox" name="OfficialReceipt2"  required="required"></td>
+<td><input type="text" class="InputBox" name="ReceivedBy2"  required="required"></td>
+<td><input type="text" class="InputBox" name="Item_Quantity2"  required="required"></td>
+<td><input type="text" class="InputBox" name="Supplier_Name2"  required="required"></td>
+<td><input type="text" class="InputBox" name="datedelivered2"  required="required"></td>
+<td><input type="text" class="InputBox" name="datereceived2"  required="required"></td>
+<td><select name="Unit" class="unit2" required="required">
+              <option value="piece">piece</option>
+              <option value="box">box</option>
+              <option value="set">set</option>
+              <option value="ream">ream</option>
+              <option value="dozen">dozen</option>
+              <option value="bundle">bundle</option>
+              <option value="sack">sack</option>
+              <option value="others">others</option>
+            </select></td>
+<td><input type="text" class="InputBox" name="Cost2" required="required"></td>
+<td><input type="text" class="InputBox" name="ExpirationDate2" required="required"></td>
 <td><input type="button" class="add" onclick="add_row();" value="Add Row"></td>
 </tr>
 
@@ -45,7 +72,7 @@
           <button type="button" class="btn btn-default" id="cancel1" data-dismiss="modal">Cancel</button>
       </div>
     </div>
-      
+      <?php echo form_close(); ?>
     </div>
   </div>
 </div>
