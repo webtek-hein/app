dd228ac0d90a6cd2de838cb75f4c4c6eb02a9504
gloridhelgoyo <gloridhelgoyo@yahoo.com>
2017-07-20 22:08:19 +0800

    item detail for department

---
+++ application/views/modals/department_item.php
@@ -0,0 +1,49 @@
+<!-- Modal-->
+<div class="modal fade" id="view" role="dialog">
+    <div class="modal-dialog" style="overflow-x:auto; width:auto ">
+        <div class="container" style="background-color:white; width:auto; height: auto; size:50px;">
+
+            <!-- Modal content-->
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title" align="center"><b>Item Details<b></h4>
+            </div>
+            <div class="modal-body" align="center">
+                <div class="box-body" style="overflow-x:auto; width:auto; ">
+                    <table id="details" class="table table-bordered table-striped" width="100%">
+                        <thead>
+                        <tr>
+                            <th>Serial #</th>
+                            <th>Expiration Date</th>
+                            <th>Supplier</th>
+                            <th>Description</th>
+                            <th>Official Receipt #</th>
+                            <th>Date Delivered</th>
+                            <th>Date Received</th>
+                            <th>Received By</th>
+                            <th>Unit Cost</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th>Serial #</th>
+                            <th>Expiration Date</th>
+                            <th>Supplier</th>
+                            <th>Description</th>
+                            <th>Official Receipt #</th>
+                            <th>Date Delivered</th>
+                            <th>Date Received</th>
+                            <th>Received By</th>
+                            <th>Unit Cost</th>
+                            <th>Action</th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
