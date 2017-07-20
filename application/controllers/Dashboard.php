2e4cc3c8c8beff996b070216af13e912b839efa2
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2017-07-20 18:08:37 +0800

    update

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,6 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
+	public function __construct()
+    {
+      	parent::__construct();
+		$this->load->model('inventorymodel');
+    }
 	public function index()
 	{
 			$this->load->view('templates/header');
@@ -9,7 +14,9 @@ class Dashboard extends CI_Controller {
 			$this->load->view('templates/footer');
 	}
 	public function graph()
-    {
-        echo json_encode("Success.");
+    {	
+    	$data = $this->inventorymodel->get_dashboard();
+        echo json_encode($data);
+
     }
 }
