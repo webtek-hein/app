0e1c2d1efa5b4fb1f8862d293583e2a897d1b5fa
Awh_Noh <markandawi@gmail.com>
2017-07-21 00:51:31 +0800

    depts

--- application/views/department_head/department.php
+++ application/views/department_head/department.php
@@ -1,7 +1,15 @@
 
 <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url() ?>assets/css/dept.css"/>
-
 <script src="<?php echo base_url() ?>assets/js/sort.js"></script>
+<?php
+    $username = ($this->session->userdata['logged_in']['username']);
+    $firstname = ($this->session->userdata['logged_in']['firstname']);
+    $lastname = ($this->session->userdata['logged_in']['lastname']);
+    $position = ($this->session->userdata['logged_in']['position']);
+    $department = ($this->session->userdata['logged_in']['department']);
+    $dept_id = ($this->session->userdata['logged_in']['dept_id']);
+
+?>
 <section class="content">
     <div class="row">
         <div class="col-xs-12">
@@ -9,9 +17,7 @@
                 <div class="box-header">
                     <select class="selectdept" name="department" id="depts">
                         <option class="option" value="none"> Departments</option>
-                        <?php foreach ($departments as $dept): ?>
-                            <option class="option" value="<?php echo $dept['dept_id'] ?>"><?php echo $dept['res_center_code'] . ' ' . $dept['department'] ?></option>
-                        <?php endforeach; ?>
+                            <option class="option" value="<?php echo $dept_id?>"><?php echo $department?></option>
                     </select>
 
 
