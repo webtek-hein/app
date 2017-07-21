b4676dd90894c3dba0dfcf68b3ad06dfb30baf88
Awh_Noh <markandawi@gmail.com>
2017-07-21 21:06:09 +0800

    fixed some for department head

--- application/views/department_head/header.php
+++ application/views/department_head/header.php
@@ -6,6 +6,7 @@ if (isset($this->session->userdata['logged_in'])) {
     $firstname = ($this->session->userdata['logged_in']['firstname']);
     $lastname = ($this->session->userdata['logged_in']['lastname']);
     $position = ($this->session->userdata['logged_in']['position']);
+	$department = ($this->session->userdata['logged_in']['department']);
 } else {
     header("location: login");
 }
@@ -59,9 +60,9 @@ if (isset($this->session->userdata['logged_in'])) {
         <!-- Logo -->
         <a href="#" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>GSO</b></span>
+            <span class="logo-mini"><b>DH</b></span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>GSO</b>Inventory</span>
+            <span class="logo-lg">Department Head</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
@@ -77,6 +78,7 @@ if (isset($this->session->userdata['logged_in'])) {
                             <!--<img src="<?php echo base_url() ?>assets/dist/img/admin.jpg" class="user-image" alt="User Image">-->
                             <span class="hidden-xs"><?= $firstname.' '.$lastname?></span>
                         </a>
+						 
                         <ul class="dropdown-menu">
                             <!-- User image -->
                             <li class="user-header">
@@ -110,8 +112,7 @@ if (isset($this->session->userdata['logged_in'])) {
                     <img src="<?php echo base_url() ?>assets/dist/img/dummy.jpg" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
-                    <p><?= $position?></p>
-                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                    <span class="logo-lg"> <?= $department ?></span>
                 </div>
             </div>
             <!-- sidebar menu: : style can be found in sidebar.less -->
