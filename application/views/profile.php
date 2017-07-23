9cb19579fa4bf34ac0591264623add99ae6d6d4f
Lovelace Zennia Luisa Oliva <2144074@slu.edu.ph>
2017-07-23 12:03:52 +0800

    profile

---
+++ application/views/profile.php
@@ -0,0 +1,99 @@
+  <div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+
+      <div class="row">
+	  <div class="col-md-3">
+	            <!-- Profile Image -->
+          <div class="box box-primary">
+            <div class="box-body box-profile">
+              <img class="profile-user-img img-responsive img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+
+              <h3 class="profile-username text-center">Admin</h3>
+			  <div class="col-sm-offset-2 col-sm-3">
+					  <button type="submit" class="btn btn-default">Choose picture</button>
+                </div>
+            </div>
+			    
+          </div>
+          <!-- /.box -->
+		</div>
+        <!-- /.col -->
+        <div class="col-md-9">
+          <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+              <li><a href="#settings" data-toggle="tab">Settings</a></li>
+            </ul>
+
+            <div class="tab-content">
+
+
+
+              </div>
+
+
+          <div class="tab-pane" id="settings">
+                <form class="form-horizontal">
+                  <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Name</label>
+
+                    <div class="col-sm-9">
+                      <input type="email" class="form-control" id="inputName" placeholder="Name">
+                    </div>
+                  </div>
+
+                 <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Last Name</label>
+
+                    <div class="col-sm-9">
+                      <input type="email" class="form-control" id="inputName" placeholder="Last name">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+
+                    <div class="col-sm-9">
+                      <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Contact no</label>
+
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="inputName" placeholder="Contact no">
+                    </div>
+                  </div>
+
+				  <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Password</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="inputName" placeholder="password">
+                    </div>
+                  </div>
+
+				   <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Confirm Password</label>
+                    <div class="col-sm-9">
+                      <input type="text" class="form-control" id="inputName" placeholder="confirm password">
+                    </div>
+                  </div>
+                  
+                  <div class="form-group">
+                    <div class="col-sm-offset-8 col-sm-9">
+					  <button type="submit" class="btn btn-success">Cancel</button>
+                      <button type="submit" class="btn btn-danger">Submit</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <!-- /.tab-pane -->
+            </div>
+            <!-- /.tab-content -->
+          </div>
+          <!-- /.nav-tabs-custom -->
+        </div>
+        <!-- /.col -->
+      </div>
+	</section>
+</div>
