06990540a056215be58c48815d000606873b5430
lyraronquillo <ljronki@gmail.com>
2017-07-28 17:07:51 +0800

    display edit log

--- application/controllers/EditLog.php
+++ application/controllers/EditLog.php
@@ -14,9 +14,28 @@ class EditLog extends CI_Controller {
         $this->load->view('editlog');
         $this->load->view('templates/footer');
 
+        $position = $this->session->userdata['logged_in']['position'];
+        $dept_id = $this->session->userdata['logged_in']['dept_id'];
+
     }
-    public function edit_log_list()
+        public function get_edit_log()
     {
+        $editlog = $this->log_model->get_edit_log();
+        $data = array();
+        foreach ($editlog as $list) {
+            $row = array();
+            $row[] = $list['before_item_name'];
+            $row[] = $list['after_item_name'];
+            $row[] = $list['before_description'];
+            $row[] = $list['after_description'];
+            $row[] = $list['before_unit'];
+            $row[] = $list['after_unit'];
+            $row[] = $list['date'];
+            $row[] = $list['user'];
+            $data[] = $row;
+        }
+        $list = array('data'=>$data);
+        echo json_encode($list);
 
     }
 }
