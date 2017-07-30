b7458f38963d66d29777e6c83b69cd99ab97337b
markr <markryanc23@gmail.com>
2017-07-30 22:33:01 +0800

    update session

--- application/views/department_head/templates/header.php
+++ application/views/department_head/templates/header.php
@@ -3,11 +3,11 @@
 <?php
 if (isset($this->session->userdata['logged_in'])) {
     $username = ($this->session->userdata['logged_in']['username']);
-    $firstname = ($this->session->userdata['logged_in']['firstname']);
-    $lastname = ($this->session->userdata['logged_in']['lastname']);
+    $firstname = ($this->session->userdata['user_in']['firstname']);
+    $lastname = ($this->session->userdata['user_in']['lastname']);
     $position = ($this->session->userdata['logged_in']['position']);
     $department = ($this->session->userdata['logged_in']['department']);
-    $image = ($this->session->userdata['logged_in']['image']);
+    $image = ($this->session->userdata['image_in']['image']);
 } else {
     header("location: login");
 }
