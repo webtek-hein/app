706b5f7002047a6e37bb21c29d024db2e3723a9e
markr <markryanc23@gmail.com>
2017-07-30 23:15:14 +0800

    details

---
+++ application/views/modals/return_details.php
@@ -0,0 +1,37 @@
+<!-- Modal-->
+<div class="modal fade" id="details" role="dialog">
+    <div class="modal-dialog" style="overflow-x:auto; width:auto ">
+        <div class="container" style="background-color:white; width:auto; height: auto; size:50px;">
+
+            <!-- Modal content-->
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title" align="center"><b>Item Details<b></h4>
+            </div>
+            <div class="modal-body" align="center">
+                <div class="box-body" style="overflow-x:auto; width:auto; ">
+                    <table id="return" class="table table-bordered table-striped" width="100%">
+                        <thead>
+                        <tr>
+                            <th>Account Code</th>
+                            <th>Serial #</th>
+                            <th>Official Receipt #</th>
+                            <th>Description</th>
+                            <th>Usage</th>
+                            <th>Distribution Date</th>
+                            <th>Received By</th>
+                            <th>Unit Cost</th>
+                            <th>Reason</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                        <tfoot>
+
+                        </tfoot>
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
