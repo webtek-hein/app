ec4b0c49ba6b41030d0d1bdaa791d228e1d0752d
JoyCabildo <Joy_Cabildo24@yahoo.com>
2017-08-06 17:55:03 +0800

    Navigated the expired items in the dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -84,7 +84,7 @@
                 <div class="icon">
                     <i class="ion ion-person-add"></i>
                 </div>
-                <span><button><a class="small-box-footer">More Details
+                <span><button><a class="small-box-footer" href='/app/inventory'>More Details
                      <i class="fa fa-arrow-circle-right"> </i></a></button> </span>
             </div>
         </div>
